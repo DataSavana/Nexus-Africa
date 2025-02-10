6a2167af0536a795f73d6a77d26344d4bde23133
Melat <mgebremeskel@wesleyan.edu>
2025-02-09 20:54:56 -0500

    webpage for our platform

---
+++ nexus_africa/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+   
+       <h1>Nexus Africa Coming Soon!...</h1>
+      
+  );
+}
+
+export default App;
