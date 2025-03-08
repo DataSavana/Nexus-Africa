b90802dfb867d9584c606cfb8a3e85c32b1fc064
Melat <mgebremeskel@wesleyan.edu>
2025-03-08 15:15:32 -0500

    Merged with Navbar change

--- nexus_africa/src/components/HeroSection.js
+++ nexus_africa/src/components/HeroSection.js
@@ -1,35 +1,54 @@
-import React from "react";
-import "./styles/HeroSection.css";
-import AuthButton from "./AuthButton";
+// import React from "react";
+// import "./styles/HeroSection.css";
+// import AuthButton from "./AuthButton";
 
-function scrollToContact(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
+// function scrollToContact(id) {
+//   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+// }
+
+// const HeroSection = () => {
+//   return (
+//     <div className="hero">
+//       <h1>Unlock Africa’s AI Data Potential</h1>
+//       <p>
+//         High-quality, ethically sourced datasets for AI companies and
+//         researchers.
+//       </p>
+//       <div className="cta-buttons">
+//         <AuthButton text="Get Started" />
+//         <button
+//           onClick={() => scrollToContact("contact")}
+//           className="secondary"
+//         >
+//           Book a Demo
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HeroSection
+
+import React from 'react'
+import digitalPulse from "../assets/DigitalPulse.mov";
 
 const HeroSection = () => {
   return (
-    <div className="hero">
-      <h1>Unlock Africa’s AI Data Potential</h1>
-      <p>
-        High-quality, ethically sourced datasets for AI companies and
-        researchers.
-      </p>
-      <div className="cta-buttons">
-        <AuthButton text="Get Started" />
-        <button
-          onClick={() => scrollToContact("contact")}
-          className="secondary"
-        >
-          Book a Demo
-        </button>
+    <div className="heroSection">
+      <video src={digitalPulse} autoPlay loop muted />
+      <div className="overlay"></div>
+      <div className="content">
+        <h1>Unlock Africa’s AI Data Potential</h1>
+        <p>
+          High-quality, ethically sourced datasets for AI companies and
+          researchers.
+        </p>
       </div>
     </div>
   );
 };
 
 export default HeroSection
-
-
 
 // import React from "react";
 // import "./styles/HeroSection.css";
